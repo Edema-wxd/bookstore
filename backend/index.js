d5ea433c2dca3e90aa41d21601ced1820f9307ed
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2024-10-23 16:08:00 +0100

    CReated and updated api

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const bookRoutes = require("./src/books/book.route");
+
+// Middleware
+app.use(express.json());
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+
+//Routes
+app.use("/api/books", bookRoutes);
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);
--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const {
+  postBook,
+  getBooks,
+  getBook,
+  updateBook,
+  deleteBook,
+} = require("./book.controller");
 const router = express.Router();
 
 // POST BOOK
+router.post("/create-book", postBook);
 
- router.post("/create-book", async() )
+// GET all books
+router.get("/", getBooks);
 
- 
+// GET a single book
+router.get("/:id", getBook);
+
+// UPDATE / PUT  a book
+router.put("/edit-book/:id", updateBook);
+
+// DELETE a book
+router.delete("/delete-book/:id", deleteBook);
+
 module.exports = router;
