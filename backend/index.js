e0993549e746e124878747657a5ee2716cb4d31a
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2024-10-25 16:58:41 +0100

    Exposing endpoint to url

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,12 @@ const adminRoutes = require("./src/stats/admin.stats");
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://bookstore-nk9f.vercel.app"],
+    credentials: true,
+  })
+);
 
 //Routes
 app.use("/api/books", bookRoutes);
